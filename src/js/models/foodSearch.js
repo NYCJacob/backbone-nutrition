68b48fa92edd690e64255d92715c829e1ed4e984
jsherman <jacob@jbsherman.com>
2017-04-06 20:46:01 -0400

    Fix: added defaults to app.Food model

--- src/js/models/foodSearch.js
+++ src/js/models/foodSearch.js
@@ -10,7 +10,13 @@ var app = app || {};
 app.Food = Backbone.Model.extend({
 
     defaults: {
-
+        item_name : '',
+        nf_calories : '',
+        nf_total_fat : '',
+        nf_total_carbohydrate : '',
+        nf_protein : '',
+        nf_serving_size_qty : '',
+        nf_serving_size_unit : ''
     }
 
 });
