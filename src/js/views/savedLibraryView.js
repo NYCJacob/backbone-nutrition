b8e56c4ee5dc02475c776d1581cb30a733e6d47c
jsherman <jacob@jbsherman.com>
2017-05-04 15:37:12 -0400

    bug: no typeerrors but saved view rendering duplicates

--- src/js/views/savedLibraryView.js
+++ src/js/views/savedLibraryView.js
@@ -17,17 +17,18 @@ $(function () {
         initialize: function () {
             this.render();
             // this.collection = new app.savedCollection();
-            // this.collection = app.savedCollection;   // savedCollection initialized in app.js
+            this.collection = app.savedCollection;   // savedCollection initialized in app.js
             this.listenTo(this.collection, 'add update change', this.render);
-            app.savedCollection.on('add', this.render);
+            // app.savedCollection.on('add', this.render);
 
         },
 
         // render library by rendering each book in its collection
         render: function () {
             // console.log('savedLibraryView render');
-            this.collection = app.savedCollection;
-            this.collection.each(function (item) {
+            // this.collection = app.savedCollection;
+            // this.collection.each(function (item) {
+            app.savedCollection.each(function (item) {
                 this.renderSaved(item);
             }, this);
         },
