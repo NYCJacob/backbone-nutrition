ec8f98387fac09e899aac6141a3d5451f19c32a7
jsherman <jacob@jbsherman.com>
2017-05-07 21:21:13 -0400

    Fea: bmi api request working

--- src/js/views/bmiView.js
+++ src/js/views/bmiView.js
@@ -29,33 +29,29 @@ $(function () {
             // using ajax rather than another collection.fetch call
             // TODO: multiple api calls with one collection?
 
-            // Make GET request to https://bmi.p.mashape.com/
+            // Make JSON request to https://bmi.p.mashape.com/
+            // could only get api to work using default metric values
             var bmiUrl = 'https://bmi.p.mashape.com/';
-            var userWeight = this.$('#bmi-weight').val();
-            var userHeight = this.$('#bmi-height').val();
+            var userWeight = (this.$('#bmi-weight').val() * .45);    // this is a truncated conversion
+            var userHeight = (this.$('#bmi-height').val() * 2.54);
             var userAge = this.$('#life-age').val();
             var userGender = this.$('#life-gender').val();
+            var urlData =  {
+                "weight":{"value": userWeight,"unit":"kg"},
+                "height":{"value": userHeight,"unit":"cm"},
+                "sex": userGender, "age": userAge};
 
+            // for json POST request see http://stackoverflow.com/questions/12693947/jquery-ajax-how-to-send-json-instead-of-querystring#12693986
             $.ajax({
                 type: 'POST',
                 url: bmiUrl,
-                dataType: 'json',
+                contentType: "application/json",
+                // dataType: 'json',
                 beforeSend: function(xhr) {
                     // xhr.setRequestHeader("X-Mashape-Authorization", "Nzn548SCl8mshyrCNNESZSV21Z8Tp1WXG8ojsn5nVLLUM88cJr");
                     xhr.setRequestHeader("X-Mashape-Authorization", "9Q2PQEr5DGmsh7Z3MtZSf4kjEOByp1IvpWajsnkN3LT1dwrFE5");
                 },
-                data: {
-                    "weight": {
-                        "value": userWeight,
-                        "unit": 'lb'
-                    },
-                    "height": {
-                        "value": userHeight,
-                        "unit": 'in'
-                    },
-                    "age": userAge,
-                    "gender": userGender
-                },
+                data: JSON.stringify(urlData),
                 done: function(data) { console.log(data); },
                 fail: function(err) { alert(err); }
             })
