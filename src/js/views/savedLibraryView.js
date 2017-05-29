f189c772514e40bd5e881b90c07792529c9523e2
jsherman <jacob@jbsherman.com>
2017-05-28 21:05:30 -0400

    Bug: findWhere save condition not working

--- src/js/views/savedLibraryView.js
+++ src/js/views/savedLibraryView.js
@@ -22,6 +22,8 @@ $(function () {
             this.$savedItems = this.$('#saved-items');
             this.$footer = this.$('#saved-footer');
             this.modelCount = 0;
+            //  array to stored saved food nutritionix index to prevent duplicate entries
+            this.savedIndex = [];
             // this.collection = new app.savedCollection();
             // this.collection = app.savedCollection;   // savedCollection initialized in app.js
             //TODO: not sure why this.collection seems to work when collection: not in constructor
@@ -60,16 +62,25 @@ $(function () {
         // save food when add clicked in library view which sends here
         saveFood: function( clicked ){
             // clicked is a regular javascript object at this point
-            // add attribute for html accordian attributes for proper templating
-            this.modelCount++;
-            clicked.modelCount = this.modelCount;
-            // add quotes to id for use in html id accordian attribute
-            clicked.href = '"' + '#collapse' + this.modelCount + '"';
-            clicked.divId = '"' + 'collapse' + this.modelCount + '"';
-            clicked.headingLabel = '"' + 'heading' + this.modelCount + '"';
-            clicked.ariaControl = '"' + 'collapse' + this.modelCount + '"';
-            // clicked object key:values map to model
-            this.collection.create( clicked );
+
+            // check for dup food based on nutritionix id
+            var exists = this.collection.findWhere( {item_id: clicked.item_id} );
+            if (typeof exists != 'undefined') {
+                // add attribute for html accordian attributes for proper templating
+                this.modelCount++;
+                clicked.modelCount = this.modelCount;
+                // add quotes to id for use in html id accordian attribute
+                clicked.href = '"' + '#collapse' + this.modelCount + '"';
+                clicked.divId = '"' + 'collapse' + this.modelCount + '"';
+                clicked.headingLabel = '"' + 'heading' + this.modelCount + '"';
+                clicked.ariaControl = '"' + 'collapse' + this.modelCount + '"';
+                // clicked object key:values map to model
+                this.collection.create( clicked );
+            }else {
+                console.log( "model exists");
+            }
+
+
         }
 
     });
