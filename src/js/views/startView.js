6f71cbeb0a50e9a0f7d09af68914cb5967085f9c
jsherman <jacob@jbsherman.com>
2017-06-06 16:46:27 -0400

    Feat: added start view

---
+++ src/js/views/startView.js
@@ -0,0 +1,32 @@
+/**
+ * Created by jsherman on 6/6/17.
+ */
+
+
+/**
+ * Created by jsherman on 5/7/17.
+ */
+
+var app = app || {};
+
+// wrap within jQuery $
+$(function () {
+    app.BmiView = Backbone.View.extend({
+        el: '#app-view',
+
+        events: {
+
+         },
+
+        initialize: function () {
+
+        },
+
+        // render bmi
+        render: function ( data ) {
+
+            return this;
+        }
+    });
+
+});
