1d838c840c41a5dbc825ea9b3f544deaae5c6bde
jsherman <jacob@jbsherman.com>
2017-06-13 21:44:39 -0400

    Fea: gulp workflow expanded

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,32 @@ var concatCss = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 var serve = require('gulp-serve');
 
+// js tasks  *********************
+//
 gulp.task('lint:js',	function	()	{
     return	gulp.src(['src/js/**/*.js', '!src/js/**/*.min'])
         .pipe(plugins.jshint())
         .pipe(plugins.jshint.reporter('jshint-stylish'))
 });
 
+gulp.task('concat-min:js', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(plugins.concat('app.min.js'))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('concat:vendor-js', function() {
+    return gulp.src(['./src/js/vendor/*min.js', './src/js/vendor/*.min.js'])
+        .pipe(plugins.concat('vendor.min.js'))
+        // .pipe(plugins.uglify())  files already minified
+        .pipe(gulp.dest('./dist/js/vendor'));
+});
+
+
+// ************************************************
+// css tasks
+// ***********************************
 gulp.task('sass',	function()	{
     return	gulp.src('src/sass/*.scss')
     // pass custom title to gulp-plumber error handler function
@@ -35,8 +55,44 @@ gulp.task('sass',	function()	{
 });
 
 gulp.task('min-css', function() {
-    return gulp.src('src/css/*.css', {base: 'src'})
-        .pipe(concatCss("css/styles.min.css"))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+    return gulp.src(['src/css/*.css', '!src/css/{vendor,vendor/*.css}'], {base: 'src'})
+        .pipe(concatCss("css/app.min.css"))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('dist'));
+});
+
+// this may not be needed because only one vendor css file and it has min
+gulp.task('vendor-css', function() {
+    return gulp.src('src/css/vendor/*.min.css', {base: 'src'})
+        .pipe(concatCss("css/vendor/vendor.min.css"))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy:css', function () {
+    return gulp.src(['src/css/vendor/*.min.css'], {
+        base: 'src'
+    }).pipe(gulp.dest('dist'));
+});
+
+// html tasks
+gulp.task('replace-min:html', function () {
+    return gulp.src('src/index.html')
+        .pipe(htmlreplace({
+            'css' : 'css/styles.min.css',
+            'js': 'scripts/app.min.js'
+        }))
+        .pipe(plugins.htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            removeEmptyAttributes: true
+        }))
+        .pipe(gulp.dest('dist'))
+});
+
+// utility functions
+// delete dist directory
+gulp.task('clean', function (done) {
+    require('del')('dist').then(function () {
+        done();
+    });
 });
