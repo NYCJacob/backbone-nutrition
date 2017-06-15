d36495908b8bfccf986812d19026df51789af548
jsherman <jacob@jbsherman.com>
2017-06-15 10:09:55 -0400

    Fix: added condition to anaylze before analysis done to avoid undefined error on .match

--- src/js/views/analyze.js
+++ src/js/views/analyze.js
@@ -43,39 +43,40 @@ $(function () {
                 app.messenger.showMessage( 'There is no BMI data.' );
             } else if ( _.isEmpty( app.nutritionData ) ) {
                 app.messenger.showMessage( 'There is no nutrition data.' );
+            } else {  // okay to analyze
+                // parse ideal weight string for upper range integer
+                // https://stackoverflow.com/questions/10003683/javascript-get-number-from-string
+                var idealRange = app.bmiData.ideal_weight.match(/\d+/g);
+                var idealWeightLower = parseInt( idealRange[0] );
+                var idealWeightUpper = parseInt( idealRange[1] );
+                var weightInt = parseInt(app.bmiData.weight.lb);
+
+                // compare ideal and actual weight
+                // negative overweight means person is overweight
+                var overWeight = idealWeightUpper - weightInt;
+                // negative underWeight means person is under weight
+                var underWeight = weightInt - idealWeightLower;
+                // weightMsg holds weight msg text
+                var weightMsg;
+                if ( overWeight < 0 ) {
+                    weightMsg = 'You are over your ideal weight by ' + Math.abs( overWeight )  + ' pounds.';
+                } else if ( underWeight < 0 ) {
+                    weightMsg = 'You are under your ideal weight by ' + Math.abs( underWeight )  + ' pounds.';
+                } else {
+                    weightMsg = 'You are within your ideal weight range.';
+                }
+
+                // compare eer calories to total calories
+
+                // send msg to render via an object for _template
+                var msgData = {};
+                msgData.weightMsg = weightMsg;
+                msgData.eer = app.bmiData.eer;
+                msgData.cals = app.nutritionData.cals;
+                self.render( msgData );
+
             }
 
-            // parse ideal weight string for upper range integer
-            // https://stackoverflow.com/questions/10003683/javascript-get-number-from-string
-            var idealRange = app.bmiData.ideal_weight.match(/\d+/g);
-            var idealWeightLower = parseInt( idealRange[0] );
-            var idealWeightUpper = parseInt( idealRange[1] );
-            var weightInt = parseInt(app.bmiData.weight.lb);
-
-            // compare ideal and actual weight
-            // negative overweight means person is overweight
-            var overWeight = idealWeightUpper - weightInt;
-            // negative underWeight means person is under weight
-            var underWeight = weightInt - idealWeightLower;
-            // weightMsg holds weight msg text
-            var weightMsg;
-            if ( overWeight < 0 ) {
-                weightMsg = 'You are over your ideal weight by ' + Math.abs( overWeight )  + ' pounds.';
-            } else if ( underWeight < 0 ) {
-                weightMsg = 'You are under your ideal weight by ' + Math.abs( underWeight )  + ' pounds.';
-            } else {
-                weightMsg = 'You are within your ideal weight range.';
-            }
-
-
-            // compare eer calories to total calories
-
-            // send msg to render via an object for _template
-            var msgData = {};
-            msgData.weightMsg = weightMsg;
-            msgData.eer = app.bmiData.eer;
-            msgData.cals = app.nutritionData.cals;
-            self.render( msgData );
 
         }
 
